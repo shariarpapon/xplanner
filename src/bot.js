0ba3526085758fb671451b828402164f1c9f0f20
Shariar Papon <spaponwork@gmail.com>
2023-03-07 20:34:41 -0500

    token reset

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const fs = require("fs");
-const {auth_token} = process.env.AUTH_TOKEN;
+const auth_token = process.env.AUTH_TOKEN;
 const {Client, Collection, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: GatewayIntentBits.Guilds })
 client.commands = new Collection();
@@ -20,7 +20,6 @@ for(const folder of function_folders)
         require(`./functions/${folder}/${file}`)(client);
     }
 }
-
 
 client.handle_events();
 client.handle_commands();
